9591fa4601823488227a8f5e758be694df758e24
Alexander-Hirsch <04alexander.hirsch@gmail.com>
2025-08-13 22:11:10 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,21 @@
+</div><!-- .col-full -->
+</div><!-- #content -->
+
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="col-full">
+
+        Footer
+    
+
+    </div><!-- .col-full -->
+</footer><!-- #colophon -->
+
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+    <?php wp_body_open(); ?>
+
+    <?php do_action('storefront_before_site'); ?>
+
+    <div id="page" class="hfeed site">
+
+        <header id="masthead" class="site-header" role="banner">
+
+            Header
+        </header><!-- #masthead -->
+
+
+
+        <div id="content" class="site-content" tabindex="-1">
+            <div class="col-full">
