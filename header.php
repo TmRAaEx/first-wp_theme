cbc29087d049e99aa1ed42ab6ae34c762a20ff92
tmraaex <tmr.aaex@gmail.com>
2025-08-16 01:13:19 +0200

    Added site logo

--- header.php
+++ header.php
@@ -21,6 +21,10 @@
         <header id="masthead" class="site-header" role="banner">
 
 
+            <a href="<?php echo esc_url(home_url("/")) ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Site logo" />
+            </a>
+
             <?php
             wp_nav_menu(array(
                 'theme_location' => 'header-menu',
